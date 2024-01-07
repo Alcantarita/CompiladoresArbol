bd1fb22494e7984be5ac45f929371807a7ceeb5f
Aldo <131095225+Alcantarita@users.noreply.github.com>
2024-01-07 10:36:30 -0600

    arbol

---
+++ src/ExprAssign.java
@@ -0,0 +1,14 @@
+public class ExprAssign extends Expression {
+    final Token name;
+    final Expression value;
+    ExprAssign(Token name, Expression value) {
+        this.name = name;
+        this.value = value;
+    }
+    @Override
+    public String toString() {
+        return "ExprAssign (" + value.toString() + ")";
+    }
+
+
+}
